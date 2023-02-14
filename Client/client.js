c4e6d9b74d71a57803f808748d73a091e79ee9d3
Tianchi Huang <htc0711@bu.edu>
2023-02-13 19:14:23 -0500

    connected frontend to backend

--- Client/client.js
+++ Client/client.js
@@ -1,6 +1,31 @@
 const form = document.querySelector('form')
+const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:8000/tweet';
+
+//loading initially none
+loadingElement.style.display = 'none';
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("Hello World");
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  const tweet = {
+    //object--inside the curly bracket
+    name,
+    content
+  }
+  console.log(tweet);
+  //form => none
+  form.style.display = 'none';
+  loadingElement.style.display = '';
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(tweet),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
 })
---
+++ Server/index.js
@@ -0,0 +1,22 @@
+//using express framework
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//url, request, and response
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hi'
+  })
+})
+
+app.post('/tweet', (req, res) => {
+  console.log(req.body);
+})
+
+app.listen(8000, () => {
+  console.log('Listening on port 8000...');
+})
