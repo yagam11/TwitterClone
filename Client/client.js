fe055078cf2abcd358dfe88090da85f2d68a68b2
Tianchi Huang <htc0711@bu.edu>
2023-02-28 17:19:48 -0500

    2/28 done

--- Client/client.js
+++ Client/client.js
@@ -2,8 +2,18 @@ const form = document.querySelector('form')
 const loadingElement = document.querySelector('.loading');
 const API_URL = 'http://localhost:8000/tweet';
 
+const listAllTweets = () => {
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((allTweets) => {
+      console.log(allTweets);
+    });
+};
+
 //loading initially none
 loadingElement.style.display = 'none';
+
+listAllTweets();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -22,10 +32,17 @@ form.addEventListener("submit", (event) => {
   loadingElement.style.display = '';
 
   fetch(API_URL, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(tweet),
     headers: {
-      'content-type': 'application/json'
-    }
-  });
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((createdTweet) => {
+      console.log(createdTweet);
+      form.reset();
+      form.style.display = "";
+      loadingElement.style.display = "none";
+    });
 })
--- Server/index.js
+++ Server/index.js
@@ -1,22 +1,64 @@
 //using express framework
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var morgan = require("morgan");
+var monk = require("monk");
+
 var app = express();
 
+var db = monk("127.0.0.1/twitter");
+
+var tweets = db.get("tweets");
+
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-//url, request, and response
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hi'
-  })
-})
+const isValidTweet = (tweet) => {
+  if (
+    tweet.name &&
+    tweet.name.toString().trim() !== "" &&
+    tweet.content &&
+    tweet.content.toString().trim() !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-app.post('/tweet', (req, res) => {
-  console.log(req.body);
-})
+//url, request, and response
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hi",
+  });
+});
+
+app.get("/tweet", (req, res) => {
+  tweets.find().then((allTweets) => {
+    res.json(allTweets);
+  });
+});
+
+app.post("/tweet", (req, res) => {
+  if (isValidTweet(req.body)) {
+    const tweet = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString(),
+      created: new Date(),
+    };
+
+    tweets.insert(tweet).then((createdTweet) => {
+      res.json(createdTweet);
+    });
+  } else {
+    res.status(422);
+    res.json({
+      message: "Name and Content are required",
+    });
+  }
+});
 
 app.listen(8000, () => {
-  console.log('Listening on port 8000...');
-})
+  console.log("Listening on port 8000 ...");
+});
